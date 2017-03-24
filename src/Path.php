0a4a4bb12340f23d8b6f5ae3c275bb1639549ff2
klucznik <klucznik@gmail.com>
2017-03-24 15:51:57 +0100

    add Path class

---
+++ src/Path.php
@@ -0,0 +1,158 @@
+<?php namespace Cog;
+
+use UnexpectedValueException;
+
+abstract class Path {
+	/**
+	 * Cog\Path of the "web root" or "document root" of the web server
+	 * Like "/home/www/htdocs" on Linux/Unix or "c:\inetpub\wwwroot" on Windows
+	 * @var string
+	 */
+	public static $webRoot;
+
+	/**
+	 * Full path of the actual PHP script being run Like "/home/www/htdocs/folder/script.php" on Linux/Unix
+	 * or "c:\inetpub\wwwroot" on Windows
+	 * @var string
+	 */
+	public static $scriptFilename;
+
+	/**
+	 * Web-relative path of the actual PHP script being run.
+	 * So for "http://www.domain.com/folder/script.php", it would be "/folder/script.php"
+	 * @var string
+	 */
+	public static $scriptName;
+
+	/** @var boolean A flag to indicate whether or not this script is run as a CLI (Command Line Interface) */
+	protected static $cliMode;
+
+	/**
+	 * This should be the first call to initialize all the static variables The application object also has
+	 * static methods that are miscellaneous web development utilities, etc.
+	 * @throws UnexpectedValueException
+	 * @return void
+	 */
+	public static function initialize() {
+		// Are we running as CLI?
+		self::$cliMode = array_key_exists('SERVER_PROTOCOL', $_SERVER) ? false : true;
+
+		if (self::$cliMode === false) {
+			self::initializeWeb();
+		} else {
+			self::initializeCli();
+		}
+	}
+
+	protected static function initializeCli() {
+		self::$webRoot = realpath(__DIR__ . '/../www');
+	}
+
+	protected static function initializeWeb() {
+		// Setup ScriptFilename and ScriptName
+		self::$scriptFilename = $_SERVER['SCRIPT_FILENAME'];
+
+		if ($_SERVER['SCRIPT_NAME']) {
+			self::$scriptName = $_SERVER['SCRIPT_NAME'];
+		} else {
+			self::$scriptName = $_SERVER['PHP_SELF'];
+		}
+
+		// Ensure both are set, or we'll have to abort
+		if (!self::$scriptFilename || !self::$scriptName) {
+			throw new UnexpectedValueException('Error on self::Initialize() - ScriptFilename or ScriptName was not set');
+		}
+
+		// Setup WebRoot -- WebRoot will NOT be set and therefore needs to be magically
+		if (array_key_exists('DOCUMENT_ROOT', $_SERVER) && $_SERVER['DOCUMENT_ROOT']) {
+			self::$webRoot = $_SERVER['DOCUMENT_ROOT'];
+		} else {
+			$firstCharacter = self::firstCharacter(self::$scriptFilename);
+
+			if ($firstCharacter === '/' || $firstCharacter === '.') { // Unix
+				// Ensure that ScriptFilename ENDS with ScriptName
+				$substrResult = strpos(self::$scriptFilename, self::$scriptName);
+				$strlenResult = strlen(self::$scriptFilename) - strlen(self::$scriptName);
+				if ($substrResult === $strlenResult) {
+					self::$webRoot = substr(self::$scriptFilename, 0, $strlenResult);
+				} else {
+					throw new UnexpectedValueException('Error on self::Initialize() - ScriptFilename does not end with ScriptName');
+				}
+			} else if (substr(self::$scriptFilename, 1, 2) === ':\\') { // Windows
+				// We need to first ascertain a DOS-compatible "Script Name"
+				$scriptName = str_replace('/', '\\', self::$scriptName);
+
+				// Ensure that ScriptFilename ENDS with ScriptName
+				$substrResult = strpos(self::$scriptFilename, $scriptName);
+				$strlenResult = strlen(self::$scriptFilename) - strlen($scriptName);
+				if ($substrResult === $strlenResult) {
+					self::$webRoot = substr(self::$scriptFilename, 0, $strlenResult);
+				} else {
+					throw new UnexpectedValueException('Error on self::Initialize() - ScriptFilename does not end with ScriptName');
+				}
+			} else {
+				// Could not ascertain Cog\FileSystem type
+				throw new UnexpectedValueException('Error on self::Initialize() - Could not ascertain file system type from ScriptFilename');
+			}
+		}
+
+		// Cleanup WebRoot -- path should not end with a trailing / or \
+		while (self::lastCharacter(self::$webRoot) === '/' || self::lastCharacter(self::$webRoot) === '\\') {
+			self::$webRoot = substr(self::$webRoot, 0, -1);
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function appRoot() {
+		return realpath(self::$webRoot . '/..');
+	}
+
+	/**
+	 * Returns true if the environment is command line
+	 * @return bool
+	 */
+	public static function isCLI() {
+		return self::$cliMode;
+	}
+
+	/**
+	 * @param $string string input string
+	 * @return string|null
+	 */
+	protected static function firstCharacter($string) {
+		if (strlen($string) > 0) {
+			return $string[0];
+		}
+		return null;
+	}
+
+	/**
+	 * @param $string string input string
+	 * @return string|null
+	 */
+	protected static function lastCharacter($string) {
+		$length = strlen($string);
+		if ($length > 0) {
+			return $string[$length - 1];
+		}
+		return null;
+	}
+
+	/**
+	 * For development purposes, this static method outputs all the Paths
+	 * @return string
+	 */
+	public final static function dump() {
+		return [
+			'appRoot' => self::appRoot(),
+			'webRoot' => self::$webRoot,
+			'scriptFilename' => self::$scriptFilename,
+			'scriptName' => self::$scriptName,
+			'cli' => self::isCLI()
+		];
+	}
+}
+
+Path::initialize();
